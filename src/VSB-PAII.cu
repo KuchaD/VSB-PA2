2b129431d2a26dfabdfa5e0d5b1b12438c7f3b60
David <kucha.david@gmail.com>
2019-02-25 21:12:31 +0100

    ADD EX 1. Part 2 New Solution

--- src/VSB-PAII.cu
+++ src/VSB-PAII.cu
@@ -33,14 +33,29 @@ __global__ void VectorAdd(int *A,int *B,int *C,int M)
 		C[i] = A[i] + B[i];
 	}
 }
-__global__
-void VectorAddMN(int *A,int *B,int *C,int M,int N)
+__global__ void VectorAddMN(int *A,int *B,int *C,int M,int N)
 {
 	const int i = blockIdx.x * blockDim.x + threadIdx.x;
 	const int j = blockIdx.y * blockDim.y + threadIdx.y;
 	if (i < M && j < N)
 	{
 		C[i * M + j] = A[i * M + j] + B[i * M + j];
+	}
+}
+
+__global__ void VectorAddMN_2(int *A,int *B,int *C,int M,int N)
+{
+	const int i = blockIdx.x * blockDim.x + threadIdx.x;
+	const int j = blockIdx.y * blockDim.y + threadIdx.y;
+	if (i < M && j < N)
+	{
+
+		for (int i = 0; i < M * N; ++i) {
+			int row = i / N;
+			int col = i % N;
+
+			C[row + col] = A[row + col] + B[row + col];
+		}
 	}
 }
 void CV1_1()
@@ -155,6 +170,79 @@ void CV1_2()
 	cudaFree(B_Device);
 	cudaFree(C_Device);
 };
+
+
+void CV1_2_2()
+{
+	const unsigned int M = 10;
+    const unsigned int N = 10;
+
+    // M rows N cols
+    int** A_Host = new int*[M];
+    int** B_Host = new int*[M];
+    int** C_Host = new int*[M];
+
+
+    A_Host[0] = new int[M * N];
+    B_Host[0] = new int[M * N];
+    C_Host[0] = new int[M * N];
+
+    for (int i = 1; i < M; ++i){
+    	A_Host[i] = A_Host[i-1] + N;
+    	B_Host[i] = B_Host[i-1] + N;
+    	C_Host[i] = C_Host[i-1] + N;
+    }
+
+    //Fill
+    for (int i = 0; i < M; ++i) {
+     for (int j = 0; j < N; ++j) {
+      A_Host[i][j] = i*j;
+      B_Host[i][j] = i*j;
+     }
+    }
+
+
+    int *A_Device;
+    cudaMalloc((void **)&A_Device, sizeof(int) * M * N);
+
+
+    int *B_Device;
+    cudaMalloc((void **)&B_Device, sizeof(int) * M * N);
+
+
+    int *C_Device;
+    cudaMalloc((void **)&C_Device, sizeof(int) * M * N);
+
+    cudaMemcpy(A_Device, A_Host[0],M*N*sizeof(int), cudaMemcpyHostToDevice);
+	cudaMemcpy(B_Device, B_Host[0], M*N*sizeof(int), cudaMemcpyHostToDevice);
+	cudaMemcpy(C_Device, C_Host[0], M*N*sizeof(int), cudaMemcpyHostToDevice);
+
+	dim3 dimBlock(M, N);
+	dim3 dimGrid(1, 1);
+
+	VectorAddMN_2<<<dimGrid, dimBlock>>>(A_Device,B_Device,C_Device,M,N);
+
+	cudaMemcpy(C_Host, C_Device, M*N*sizeof(int), cudaMemcpyDeviceToHost);
+
+
+	for (int i = 0; i < M; i++)
+		{
+			std::cout << i << "| " ;
+			for (int j = 0; j < N; j++)
+			{
+				std::cout << C_Host[i][j] << "   ";
+			}
+			std::cout << std::endl;
+		}
+
+	free(A_Host);
+	free(B_Host);
+	free(C_Host);
+	cudaFree(A_Device);
+	cudaFree(B_Device);
+	cudaFree(C_Device);
+};
+
 int main(int argc, char *argv[])
 {
 
